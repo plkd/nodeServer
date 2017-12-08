292478aa26dc299151db5651b559c4c3f5e31747
plkd <209603537@qq.com>
2017-12-08 14:00:57 +0800

    createToken

--- middleware/createToken.js
+++ middleware/createToken.js
@@ -1,3 +1,13 @@
 /**
  * Created by lijiliang on 2017/12/8.
  */
+let jwt = require('jsonwebtoken');
+module.exports = function (name) {
+  let expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+  const token = jwt.sign({
+    name: name,
+    exp: parseInt(expiry.getTime() / 1000)
+  }, process.env.JWT_SECRET)
+  return token
+}
